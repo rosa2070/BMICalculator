81fb75f79ebb30f9cc18582e8dd27225d8aebb22
jaeeunhyun <rosa2070@naver.com>
2022-01-23 16:04:28 +0900

    df

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
